e6627c397d9e3b0cb03ab9bcb428920e77fad0d6
Isaac Gibbs <igibbs@OSTX.com>
2016-09-12 15:46:44 -0400

    add mongo, and scaffolding

--- client/main.jsx
+++ client/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import App from '../imports/ui/app.jsx';
+import '..imports/api/picks.js';
 
 Meteor.startup(() => {
 
---
+++ imports/api/picks.js
@@ -0,0 +1,3 @@
+import {Mongo} from 'meteor/mongo';
+
+export const Picks = new Mongo.Collection('picks');
